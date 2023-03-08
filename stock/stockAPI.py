d21de28aaf766dedf998fab75d44dc0f6e0ad2c7
Migueljfc <miguel.f.cabral@ua.pt>
2023-03-08 18:19:06 +0000

    ✅ requitements.txt

--- stock/stockAPI.py
+++ stock/stockAPI.py
@@ -28,7 +28,6 @@ class Product(BaseModel):
     description: str = Field("", description="The description of the product")
     id: UUID = Field(..., description="The ID of the product")
     category_id: UUID = Field(..., description="The ID of the category that the product belongs to")
-    amount: int = Field(..., description="The amount of the product in stock")
     image: str = Field("", description="The image url of the product")
 
 
@@ -36,16 +35,12 @@ class NewProduct(BaseModel):
     name: str = Field(..., description="The name of the new product")
     description: str = Field("", description="The description of the new product")
     category_id: UUID = Field(..., description="The ID of the category that the new product belongs to")
-    amount: int = Field(..., description="The amount of the product in stock")
     image: str = Field("", description="The image url of the new product")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Server is UP"}
 
-@app.get("/loadfiles")
-async def load_files():
+@app.on_event("startup")
+async def startup_event():
     with open('categories.csv', 'r') as f:
         reader = csv.reader(f)
         for row in reader:
@@ -63,19 +58,23 @@ async def load_files():
             name = row[1]
             description = row[2]
             category_id = row[3]
-            amount = row[4]
-            image = row[5]
-            product = Product(id=product_id, name=name, description=description, category_id=category_id, amount=amount, image=image)
+            image = row[4]
+            product = Product(id=product_id, name=name, description=description, category_id=category_id, image=image)
             products.append(product)
             print(product)
-    return {"message": "Files loaded"}
 
-@app.get("/categories")
+
+@app.get("/")
+async def root():
+    return {"message": "Server is UP"}
+
+
+@app.get("/v1/categories",tags=["categories"])
 async def get_categories(limit: int = Query(50, ge=1, le=50), offset: int = Query(0, ge=0)):
     return categories[offset:offset+limit]
 
 
-@app.post("/categories", response_model=Category, status_code=201)
+@app.post("/v1/categories", response_model=Category, status_code=201,tags=["categories"])
 async def create_category(new_category: NewCategory):
     category_id = (uuid.uuid4())
     category = Category(id=category_id, name=new_category.name, image=new_category.image)
@@ -87,7 +86,7 @@ async def create_category(new_category: NewCategory):
     return category
 
 
-@app.get("/categories/{categoryId}", response_model=Category)
+@app.get("/v1/categories/{categoryId}", response_model=Category,tags=["categories"])
 async def get_category(categoryId: UUID):
     for category in categories:
         if category.id == categoryId:
@@ -95,7 +94,7 @@ async def get_category(categoryId: UUID):
     raise HTTPException(status_code=404, detail="Category not found")
 
 
-@app.delete("/categories/{categoryId}", status_code=204)
+@app.delete("/v1/categories/{categoryId}", status_code=204,tags=["categories"])
 async def delete_category(categoryId: UUID):
     for i, category in enumerate(categories):
         if category.id == categoryId:
@@ -104,7 +103,7 @@ async def delete_category(categoryId: UUID):
     raise HTTPException(status_code=404, detail="Category not found")
 
 
-@app.put("/categories/{categoryId}", response_model=Category)
+@app.put("/v1/categories/{categoryId}", response_model=Category,tags=["categories"])
 async def update_category(categoryId: UUID, updated_category: Category):
     for i, c in enumerate(categories):
         if c.id == categoryId:
@@ -112,45 +111,43 @@ async def update_category(categoryId: UUID, updated_category: Category):
             return update_category
         
     raise HTTPException(status_code=404, detail="Category not found")
-
-@app.get("/categories/{categoryId}/products", response_model=Product)
-async def get_products_by_category(categoryId: UUID,limit: int = Query(50, ge=1, le=50), offset: int = Query(0, ge=0)):
-    filtered_products = []
-    for product in products:
-        print(product)
-        if product.category_id == categoryId:
-            filtered_products.append(product)
-    return filtered_products[offset:offset+limit]
             
 
-@app.get("/products")
-async def get_products(categoryID: UUID = None, limit: int = Query(50, ge=1, le=50), offset: int = Query(0, ge=0)):
+@app.get("/v1/products",tags=["products"])
+async def get_products(categoryId: UUID = None, limit: int = Query(50, ge=1, le=50), offset: int = Query(0, ge=0),tags=["products"]):
+    if categoryId:
+        filtered_products = []
+        for product in products:
+            if product.category_id == categoryId:
+                filtered_products.append(product)
+        return filtered_products[offset:offset+limit]
     return products[offset:offset+limit]
 
 
-@app.post("/products", response_model=Product, status_code=201)
+@app.post("/v1/products", response_model=Product, status_code=201,tags=["products"])
 async def create_product(new_product: NewProduct):
     product_id = (uuid.uuid4())
-    product = Product(id=product_id, name=new_product.name, description=new_product.description, category_id=new_product.category_id, amount=new_product.amount, image= new_product.image)
+    product = Product(id=product_id, name=new_product.name, description=new_product.description, category_id=new_product.category_id, image= new_product.image)
     products.append(product)
     print(product)
     with open('products.csv', 'a') as f:
         writer = csv.writer(f)
-        writer.writerow([product_id, new_product.name, new_product.description, new_product.category_id, new_product.amount])
+        writer.writerow([product_id, new_product.name, new_product.description, new_product.category_id, new_product.image])
     return product
 
 
-@app.get("/products/{productId}", response_model=Product)
+@app.get("/v1/products/{productId}", response_model=Product,tags=["products"])
 async def get_product(productId: UUID):
     for product in products:
         if product.id == productId:
             return product
     raise HTTPException(status_code=404, detail="Product not found")
 
-@app.delete("/products/{productId}", status_code=204)
+@app.delete("/v1/products/{productId}", status_code=204,tags=["products"])
 async def delete_product(productId: UUID):
     for i, product in enumerate(products):
         if product.id == productId:
             del products[i]
             return
     raise HTTPException(status_code=404, detail="Product not found")
+
