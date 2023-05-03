3fdb613c0fdab7e887f8966853a72bc34ea6ab78
Gil <82401775+GilFernandes2000@users.noreply.github.com>
2023-05-03 16:17:31 +0100

    frontend in docker success

--- frontend/app/database.py
+++ frontend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///../db/database.db"
 # TODO: Change to MySQL for production
 
 engine = create_engine(
