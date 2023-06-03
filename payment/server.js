ccf425b98ca9fbcf970ab830842244938e0f8669
Carlos Costa <carlospalmacosta@gmail.com>
2023-06-03 15:56:23 +0100

    some changes to payment

--- payment/server.js
+++ payment/server.js
@@ -2,9 +2,12 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 app.set("view engine", "ejs")
 app.use(express.static('public'))
 app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 const paypal = require('@paypal/checkout-server-sdk')
 const Environment = process.env.NODE_ENV === 'production' ? paypal.core.LiveEnvironment : paypal.core.SandboxEnvironment
@@ -15,76 +18,34 @@ app.get('/payment/', (req, res) => {
 })
 
 app.post('/payment/create-order', async (req, res) => {
-    const { items } = req.body;
 
-    const itemList = items.map((item) => ({
-        name: item.name,
-        sku: item.sku,
-        price: item.price,
-        currency: item.currency,
-        quantity: item.quantity,
-    }));
-
-    const totalAmount = items.reduce((total, item) => {
-        const itemAmount = parseFloat(item.price) * parseInt(item.quantity);
+    const totalAmount = req.body.items.reduce((total, item) => {
+        const itemAmount = parseFloat(item.price) * parseInt(item.quantity, 10);
         return total + itemAmount
     }, 0);
-    
-    const createPaymentJson = {
-        intent: 'sale',
-        payer: {
-            payment_method: 'paypal',   // ?? talvez obriga a meter pagamento com paypal
-        },
-        redirect_urls: {
-            return_url: '/success',
-            cancel_url: '/cancel',
-        },
-        transactions: [
+
+    const request = new paypal.orders.OrdersCreateRequest()
+    request.prefer("return=representation")
+    request.requestBody( {
+        intent: 'CAPTURE',
+        purchase_units: [
             {
-                item_list: {
-                    items: itemList,
-                },
-                amonut: {
-                    currency: 'EUR',
-                    total: totalAmount.toFixed(2),
+                amount: {
+                    currency_code: 'EUR',
+                    value: totalAmount.toFixed(2),
                 },
                 description: 'Payment description',
+                items: req.body.items,
             },
         ],
-    };
-
-    paypal.payment.create(createPaymentJson, (error, payment) => {
-        if (error) {
-          throw error;
-        } else {
-          // Redirect user to PayPal for approval
-          res.redirect(payment.links[1].href);
-        }
-      });
-})
-    
-app.get('/success', (req, res) => {
-    const payerId = req.query.PayerID;
-    const paymentId = req.query.paymentId;
-      
-    const executePaymentJson = {
-        payer_id: payerId,
-    };
-      
-    paypal.payment.execute(paymentId, executePaymentJson, (error, payment) => {
-        if (error) {
-            throw error;
-        } else {
-            // Payment successful, handle the transaction
-            res.send('Payment successful!');
-
-        }
     });
-});
-      
-app.get('/cancel', (req, res) => {
-    // Handle cancellation
-    res.send('Payment cancelled!');
-});
+
+    try{
+        const order = await paypalClient.execute(request)
+        res.json({ id: order.result.id})
+    } catch(e) {
+        res.status(500).json({ error: e.message })
+    }
+})
 
 app.listen(4000)
