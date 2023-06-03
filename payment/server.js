bfb2d3d10790b19a4a5dd62e56090e1ff8b19505
Carlos Costa <carlospalmacosta@gmail.com>
2023-06-03 17:20:41 +0100

    payment done

--- payment/server.js
+++ payment/server.js
@@ -2,43 +2,57 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser');
 app.set("view engine", "ejs")
 app.use(express.static('public'))
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
 
 const paypal = require('@paypal/checkout-server-sdk')
 const Environment = process.env.NODE_ENV === 'production' ? paypal.core.LiveEnvironment : paypal.core.SandboxEnvironment
 const paypalClient = new paypal.core.PayPalHttpClient(new Environment(process.env.PAYPAL_CLIENT_ID, process.env.PAYPAL_CLIENT_SECRET))
 
-app.get('/payment/', (req, res) => {
+// const storeItems = new Map([
+//     [1, { price: 10, name: "yeet"}],
+//     [2, { price: 20, name: "yeeeeeet"}],
+// ])
+
+app.get('/', (req, res) => {
     res.render('index', { paypalClientId: process.env.PAYPAL_CLIENT_ID,})
 })
 
-app.post('/payment/create-order', async (req, res) => {
-
-    const totalAmount = req.body.items.reduce((total, item) => {
-        const itemAmount = parseFloat(item.price) * parseInt(item.quantity, 10);
-        return total + itemAmount
-    }, 0);
-
+app.post('/create-order', async (req, res) => {
     const request = new paypal.orders.OrdersCreateRequest()
+    const total = req.body.items.reduce((sum, item) => {
+        return sum + item.price * item.quantity
+    }, 0)
     request.prefer("return=representation")
-    request.requestBody( {
+    request.requestBody({
         intent: 'CAPTURE',
         purchase_units: [
             {
                 amount: {
-                    currency_code: 'EUR',
-                    value: totalAmount.toFixed(2),
+                    currency_code: 'USD',
+                    value: total,
+                    breakdown: {
+                        item_total: {
+                            currency_code: "USD",
+                            value: total
+                        }
+                    }
                 },
-                description: 'Payment description',
-                items: req.body.items,
-            },
-        ],
-    });
+                items: req.body.items.map(item => {
+                    //const storeItem = storeItems.get(item.id)
+                    return{
+                        name: item.name,
+                        unit_amount: {
+                            currency_code: "USD",
+                            value: item.price
+                        },
+                        quantity: item.quantity
+                    }
+                })
+            }
+        ]
+    })
 
     try{
         const order = await paypalClient.execute(request)
