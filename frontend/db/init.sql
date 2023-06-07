5c2be478b6c82976bd0940dc9c845d0a2ea7efbc
João <jtsimoes@ua.pt>
2023-06-07 02:18:48 +0100

    Changed username for database on frontend

--- frontend/db/init.sql
+++ frontend/db/init.sql
@@ -1,8 +1,8 @@
 DROP DATABASE IF EXISTS ressellr;
 CREATE DATABASE ressellr;
 
-CREATE OR REPLACE USER 'user'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON ressellr.* TO 'user'@'localhost';
+CREATE OR REPLACE USER 'frontend'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON ressellr.* TO 'frontend'@'localhost';
 
 USE ressellr;
 
@@ -45,13 +45,13 @@ CREATE TABLE items (
 
 
 -- Insert random users
-INSERT INTO users (token, username, name, email, avatar, rating, total_sales, total_purchases, total_reviews, phone, registration, location, is_active)
+INSERT INTO users (`token`, `username`, `name`, `email`, `avatar`, `rating`, `total_sales`, `total_purchases`, `total_reviews`, `phone`, `registration`, `location`, `is_active`)
 VALUES
     ('token1', 'user1', 'User One', 'user1@example.com', 'avatar1.jpg', 4.5, 10, 8, 12, 123456789, NOW(), 'Location 1', TRUE),
     ('token2', 'user2', 'User Two', 'user2@example.com', 'avatar2.jpg', 3.8, 5, 15, 6, 987654321, NOW(), 'Location 2', TRUE);
 
 -- Insert random items
-INSERT INTO items (title, date, description, price, old_price, image, location, condition, is_available, owner_id, category_id, product_id)
+INSERT INTO items (`title`, `date`, `description`, `price`, `old_price`, `image`, `location`, `condition`, `is_available`, `owner_id`, `category_id`, `product_id`)
 VALUES
     ('Item 1', NOW(), 'Description for Item 1', 50.99, 39.99, 'item1.jpg', 'Location 1', 'Good', TRUE, 1, 1, 1),
     ('Item 2', NOW(), 'Description for Item 2', 24.99, NULL, 'item2.jpg', 'Location 2', 'Excellent', TRUE, 1, 2, 2),
